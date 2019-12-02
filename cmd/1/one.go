30d51627935677bc77907c33b2923c260708e1e6
fwwieffering <fwieffering@gmail.com>
2019-12-02 13:13:17 -0600

    day one part two

--- cmd/1/one.go
+++ cmd/1/one.go
@@ -125,6 +125,14 @@ var input = `51585
 //     For a mass of 100756, the fuel required is 33583.
 // The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
 // What is the sum of the fuel requirements for all of the modules on your spacecraft?
+// --- Part Two ---
+// During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+// Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+// So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+//     A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+//     At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+//     The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+// What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
 var One = &cobra.Command{
 	Use:   "one",
 	Short: "one runs advent of code problem one https://adventofcode.com/2019/day/1",
@@ -137,15 +145,18 @@ func one(cmd *cobra.Command, args []string) {
 	scanner.Split(bufio.ScanLines)
 
 	total := 0
+	totalIncludingFuel := 0
 	for scanner.Scan() {
 		txt := scanner.Text()
 		i, err := strconv.Atoi(txt)
 		if err != nil {
 			logger.Fatalf("Bad input: unable to convert %s to integer: %s", txt, err.Error())
 		}
-		total += getFuleReq(i)
+		total += getFuelReq(i)
+		totalIncludingFuel += getFuelReqIncludingFuel(i)
 	}
-	logger.Infof("Total Fuel requirement: %d", total)
+	logger.Infof("(part one) Total Fuel requirement for modules, not including the fuel requirement for the fuel itself: %d", total)
+	logger.Infof("(part two) Total Fuel requirement for modules: %d", totalIncludingFuel)
 }
 
 // Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
@@ -154,8 +165,25 @@ func one(cmd *cobra.Command, args []string) {
 //     For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
 //     For a mass of 1969, the fuel required is 654.
 //     For a mass of 100756, the fuel required is 33583.
-func getFuleReq(i int) int {
+func getFuelReq(i int) int {
 	// golang integer division automatically rounds down
 	divideThree := i / 3
 	return divideThree - 2
+}
+
+// So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+//     A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+//     At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+//     The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346
+func getFuelReqIncludingFuel(moduleWeight int) int {
+	var total = 0
+
+	var current = getFuelReq(moduleWeight)
+
+	for current > 0 {
+		total += current
+		current = getFuelReq(current)
+	}
+
+	return total
 }
--- cmd/1/one_test.go
+++ cmd/1/one_test.go
@@ -26,9 +26,36 @@ func TestGetFuelReq(t *testing.T) {
 	}
 
 	for _, c := range testCases {
-		res := getFuleReq(c.in)
+		res := getFuelReq(c.in)
 		if res != c.out {
 			t.Fatalf("getFuleReq(%d) should equal %d. Got: %d", c.in, c.out, res)
 		}
 	}
+}
+
+func TestGetFuelReqIncludingFuel(t *testing.T) {
+	var testCases = []struct {
+		in  int
+		out int
+	}{
+		{
+			in:  14,
+			out: 2,
+		},
+		{
+			in:  1969,
+			out: 966,
+		},
+		{
+			in:  100756,
+			out: 50346,
+		},
+	}
+	for _, c := range testCases {
+		res := getFuelReqIncludingFuel(c.in)
+		if res != c.out {
+			t.Fatalf("getFuelReqIncludingFuel(%d) should equal %d. Got: %d", c.in, c.out, res)
+		}
+	}
+
 }
